88c08009b0d1bca563665ba2c3a8ccbf5bdbdad7
crdx <pub@crdx.org>
2022-12-15 22:20:52 +0000

    Format import statements

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/crdx/mission/config"
-	"github.com/crdx/mission/logger"
-	"github.com/crdx/mission/util"
 	"log"
 	"os"
 	"path"
 	"time"
+
+	"github.com/crdx/mission/config"
+	"github.com/crdx/mission/logger"
+	"github.com/crdx/mission/util"
 
 	"github.com/crdx/col"
 	"github.com/crdx/duckopt"
